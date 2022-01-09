a9b0d5ca028f99ed110d62bb6084b2118c5fd47c
evgenijjj78 <78665356+evgenijjj78@users.noreply.github.com>
2022-01-09 13:38:58 +0300

    Update PlayerServiceImpl.java

--- src/main/java/com/game/service/PlayerServiceImpl.java
+++ src/main/java/com/game/service/PlayerServiceImpl.java
@@ -20,37 +20,22 @@ public class PlayerServiceImpl implements PlayerService {
 	
 	@Autowired
 	private PlayerRepository repository;
-	private List<Player> players;
 	private List<Player> filteredPlayers;
-	private PlayerOrder order;
-	private Integer pageSize;
-	private String name, title;
-	private Race race;
-	private Profession profession;
-	private Long after, before;
-	private Boolean banned;
-	private Integer minExperience, maxExperience, minLevel, maxLevel;
 
 	@Override
 	public List<Player> getPlayers(String name, String title, Race race, Profession profession, Long after, Long before, Boolean banned
 			, Integer minExperience, Integer maxExperience, Integer minLevel, Integer maxLevel, PlayerOrder order, Integer pageNumber
 			, Integer pageSize) {
 		
-		if (players == null) fillPlayers();
-		
-		if (this.name != name || this.title != title || this.race != race || this.profession != profession || this.after != after 
-				|| this.before != before || this.banned != banned || this.minExperience != minExperience || this.maxExperience != maxExperience
-				|| this.minLevel != minLevel || this.maxLevel != maxLevel) 
 			filteredPlayers(name, title, race, profession, after, before, banned, minExperience, maxExperience, minLevel, maxLevel);
 		
 		order = order != null ? order : PlayerOrder.ID;
-		if (pageSize != null) this.pageSize = pageSize;
-		if (this.pageSize == null) this.pageSize = 3;
-		if (this.order != order) sortedPlayers(order);
+		sortedPlayers(order);
+		pageSize = pageSize == null ? pageSize = 3 : pageSize;
 		int playersCount = filteredPlayers.size();
-		int fromIndex = pageNumber != null ? pageNumber * this.pageSize : 0;
+		int fromIndex = pageNumber != null ? pageNumber * pageSize : 0;
 		fromIndex = fromIndex <= playersCount ? fromIndex : playersCount;
-		int toIndex = fromIndex + this.pageSize;
+		int toIndex = fromIndex + pageSize;
 		toIndex = toIndex > playersCount ? playersCount : toIndex;
 		return filteredPlayers.subList(fromIndex, toIndex);
 	}
@@ -59,18 +44,15 @@ public class PlayerServiceImpl implements PlayerService {
 	public int getCount(String name, String title, Race race, Profession profession
 			, Long after, Long before, Boolean banned, Integer minExperience
 			, Integer maxExperience, Integer minLevel, Integer maxLevel) {
-		
-		if (this.name != name || this.title != title || this.race != race || this.profession != profession || this.after != after 
-				|| this.before != before || this.banned != banned || this.minExperience != minExperience || this.maxExperience != maxExperience
-				|| this.minLevel != minLevel || this.maxLevel != maxLevel) 
-			filteredPlayers(name, title, race, profession, after, before, banned, minExperience, maxExperience, minLevel, maxLevel);
+		filteredPlayers(name, title, race, profession, after, before, banned, minExperience, maxExperience, minLevel, maxLevel);
 		return filteredPlayers.size();
 	}
 	
 	@Override
-	public Player getPlayerById(long id) throws PlayerNotFoundException {
-		for (Player player : players) {
-			if (player.getId() == id) return player;
+	public Player getPlayerById(Long id) throws PlayerNotFoundException, PlayerValidationException {
+		if (id == null || id == 0) throw new PlayerValidationException();
+		for (Player player : repository.findAll()) {
+			if (player.getId().longValue() == id) return player;
 		}
 		throw new PlayerNotFoundException();
 	}
@@ -81,12 +63,11 @@ public class PlayerServiceImpl implements PlayerService {
 		player.setLevel((int) ((Math.sqrt(2500 + 200 * player.getExperience()) - 50) / 100));
 		player.setUntilNextLevel(50 * (player.getLevel() + 1) * (player.getLevel() + 2) - player.getExperience());
 		Player createdPlayer = repository.save(player);
-		fillPlayers();
 		return createdPlayer;
 	}
 	
 	@Override
-	public Player update(Player player, long id) throws PlayerValidationException, PlayerNotFoundException {
+	public Player update(Player player, Long id) throws PlayerValidationException, PlayerNotFoundException {
 		Player updatedPlayer = getPlayerById(id);
 		if (player.getName() != null) updatedPlayer.setName(player.getName());
 		if (player.getTitle() != null) updatedPlayer.setTitle(player.getTitle());
@@ -98,79 +79,62 @@ public class PlayerServiceImpl implements PlayerService {
 			updatedPlayer.setExperience(player.getExperience());
 			updatedPlayer.setLevel((int) ((Math.sqrt(2500 + 200 * player.getExperience()) - 50) / 100));
 			updatedPlayer.setUntilNextLevel(50 * (updatedPlayer.getLevel() + 1) * (updatedPlayer.getLevel() + 2) - player.getExperience());
+			validatePlayer(updatedPlayer);
+			repository.save(updatedPlayer);
 		}
-		validatePlayer(player);
-		repository.save(updatedPlayer);
-		fillPlayers();
 		return updatedPlayer;
 	}
 	
 	@Override
-	public void delete(long id) throws PlayerNotFoundException {
-		repository.delete(getPlayerById(id));
-		fillPlayers();
+	public void delete(long id) throws PlayerNotFoundException, PlayerValidationException {
+		getPlayerById(id);
+		repository.deleteById(id);
 	}
 	
 	private void validatePlayer(Player player) throws PlayerValidationException {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy");
-		if (player.getName().isEmpty() || player.getName().length() > 12 || player.getTitle().isEmpty() || player.getTitle().length() > 30 
-				|| player.getBirthday() == null || Integer.parseInt(sdf.format(player.getBirthday())) < 2000 
-				|| Integer.parseInt(sdf.format(player.getBirthday())) > 3000 || player.getExperience() == null || player.getExperience() < 0 
-				|| player.getExperience() > 10000000) throw new PlayerValidationException();
-	}
-	
-	private void fillPlayers() {
-		players = new ArrayList<>();
-		filteredPlayers = new ArrayList<>();
-		players.addAll((List<Player>) repository.findAll());
-		filteredPlayers.addAll(players);
-		order = PlayerOrder.ID;
+		if (player.getName() == null || player.getName().isEmpty() || player.getName().length() > 12 || player.getTitle() == null 
+				|| player.getTitle().isEmpty() || player.getTitle().length() > 30 || player.getBirthday() == null 
+				|| Integer.parseInt(sdf.format(player.getBirthday())) < 2000 || Integer.parseInt(sdf.format(player.getBirthday())) > 3000 
+				|| player.getExperience() == null || player.getExperience() < 0 || player.getExperience() > 10000000) 
+			throw new PlayerValidationException();
 	}
 	
 	private void filteredPlayers(String name, String title, Race race, Profession profession
 			, Long after, Long before, Boolean banned, Integer minExperience
 			, Integer maxExperience, Integer minLevel, Integer maxLevel) {
 		filteredPlayers = new ArrayList<>();
-		if(name != null || title != null || race != null || profession != null || after != null || before != null 
-				|| minExperience != null || maxExperience != null || minLevel != null || maxLevel != null) {
-			for (Player player : players) {
-				if (name != null && !player.getName().contains(name)) continue;
-				if (title != null && !player.getTitle().contains(title)) continue;
-				if (race != null && player.getRace() != race) continue;
-				if (profession != null && player.getProfession() != profession) continue;
-				if (after != null && player.getBirthday().getTime() <= after) continue;
-				if (before != null && player.getBirthday().getTime() >= before) continue;
-				if (banned != null && player.getBanned() != banned) continue;
-				if (minExperience != null && player.getExperience() < minExperience) continue;
-				if (maxExperience != null && player.getExperience() > maxExperience) continue;
-				if (minLevel != null && player.getLevel() < minLevel) continue;
-				if (maxLevel != null && player.getLevel() > maxLevel) continue;
-				filteredPlayers.add(player);
-			}
-		} else filteredPlayers.addAll(players);
-		this.name = name; this.title = title; this.race = race; this.profession = profession;
-		this.after = after; this.before = before; this.banned = banned; this.minExperience = minExperience;
-		this.maxExperience = maxExperience; this.minLevel = minLevel; this.maxLevel = maxLevel;
+		for (Player player : repository.findAll()) {
+			if (name != null && !player.getName().contains(name)) continue;
+			if (title != null && !player.getTitle().contains(title)) continue;
+			if (race != null && player.getRace() != race) continue;
+			if (profession != null && player.getProfession() != profession) continue;
+			if (after != null && player.getBirthday().getTime() <= after) continue;
+			if (before != null && player.getBirthday().getTime() >= before) continue;
+			if (banned != null && !player.getBanned().equals(banned)) continue;
+			if (minExperience != null && player.getExperience() < minExperience) continue;
+			if (maxExperience != null && player.getExperience() > maxExperience) continue;
+			if (minLevel != null && player.getLevel() < minLevel) continue;
+			if (maxLevel != null && player.getLevel() > maxLevel) continue;
+			filteredPlayers.add(player);
+		}
 	}
 	
 	private void sortedPlayers(PlayerOrder order) {
-		if (this.order != order) { 
-			Collections.sort(filteredPlayers, new Comparator<Player>() {
-				@Override 
-				public int compare(Player player1, Player player2) {
-					switch (order) {
-						case NAME:
-							return player1.getName().compareTo(player2.getName());
-						case EXPERIENCE:
-							return player1.getExperience().compareTo(player2.getExperience());
-						case BIRTHDAY:
-							return player1.getBirthday().compareTo(player2.getBirthday());
-						default:
-							return player1.getId().compareTo(player2.getId());
-					}
+		Collections.sort(filteredPlayers, new Comparator<Player>() {
+			@Override 
+			public int compare(Player player1, Player player2) {
+				switch (order) {
+					case NAME:
+						return player1.getName().compareTo(player2.getName());
+					case EXPERIENCE:
+						return player1.getExperience().compareTo(player2.getExperience());
+					case BIRTHDAY:
+						return player1.getBirthday().compareTo(player2.getBirthday());
+					default:
+						return player1.getId().compareTo(player2.getId());
 				}
-			});
-			this.order = order;
-		}
+			}
+		});
 	}
 }
